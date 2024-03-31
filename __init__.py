6d33537bed5813fb0ff0360b3c3dc092d9afb36d
Samer Aldhaher <samerps@gmail.com>
2024-03-31 20:14:09 +0100

    addon creation

---
+++ __init__.py
@@ -0,0 +1,50 @@
+bl_info = {
+    "name": "BlenderFH",
+    "author": "Samer Aldhaher",
+    "version": (1, 0),
+    "blender": (4, 00, 0),
+    "location": "View3D > Add > Mesh > New Object",
+    "description": "Blender Fast Henry interface",
+    "warning": "",
+    "doc_url": "",
+    "category": "Add Mesh",
+}
+
+
+import bpy
+
+if "bpy" in locals():
+	# bring in reload library
+    from importlib import reload
+	# reload using function
+    pass
+
+
+def menu_func(self, context):
+    pass
+
+# Registration
+
+blender_classes = []
+
+
+
+# This allows you to right click on a button and link to documentation
+#def add_object_manual_map():
+#    url_manual_prefix = "https://docs.blender.org/manual/en/latest/"
+#    url_manual_mapping = (
+#        ("bpy.ops.mesh.add_object", "scene_layout/object/types.html"),
+#    )
+#    return url_manual_prefix, url_manual_mapping
+
+
+def register():
+    pass
+
+
+def unregister():
+    pass
+
+
+if __name__ == "__main__":
+    register()
